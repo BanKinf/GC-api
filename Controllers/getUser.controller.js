fece4a73dc447af38310719cc159d2c446fff27e
_ignacio.dev_ <86930603+BanKinf@users.noreply.github.com>
2023-07-23 02:41:18 -0300

    updating

--- Controllers/getUser.controller.js
+++ Controllers/getUser.controller.js
@@ -1,9 +1,8 @@
 const getUser = async (req, res) => {
   const idReq = req.params.id;
-  async function getDataPlayerById(id) {
     try {
       const response = await fetch(
-        `https://gamersclub.com.br/api/box/init/${id}`
+        `https://gamersclub.com.br/api/box/init/${idReq}`
       );
       if (!response.ok) {
         throw new Error("No se pudo obtener los datos del jugador");
@@ -17,23 +16,15 @@ const getUser = async (req, res) => {
         calibrationMatches: data.playerInfo.calibrationMatches
       };
 
-      return playerData;
-    } catch (e) {
-      return null;
-    }
-  }
-
-  try {
-    const playerData = await getDataPlayerById(idReq);
-    if (playerData.nick === "") {
+      if (playerData.nick === "") {
         res.status(404).json({message: "Player not found"}) 
-    } else  {
+      } else  {
         res.status(200).json(playerData)
+      }
+    } catch (e) {
+      res.status(404).json({message: "Input is not id"})
     }
-  } catch (e) {
-    res.status(404).json({message: "Input is not id"})
-  }
-};
+}
 
 module.exports = {
   getUser,
